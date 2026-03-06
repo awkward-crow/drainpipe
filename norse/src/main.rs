192f52348c10f804511d06a18970229aa4f53c97
awkward crow <awkward.crow@gmail.com>
2026-03-06 12:37:04 +0000

    better use of polars

--- norse/src/main.rs
+++ norse/src/main.rs
@@ -2,17 +2,19 @@
 
 use regex::Regex;
 use std::fs;
+use std::env;
 
 use polars::chunked_array::ops::SortMultipleOptions;
-use polars::datatypes::AnyValue::{String, UInt32};
-use polars::frame::{column::Column, row::Row, DataFrame};
+use polars::frame::{column::Column, DataFrame};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filename = env::args().nth(1).expect("usage: norse <filename>");
+
     let s = fs::read_to_string("./stop_words.txt").expect("this should be ok");
     let stop_words = s.split(",").collect::<Vec<&str>>();
 
-    let data = fs::read_to_string("./pride-and-prejudice.txt")
-        .expect("can't find p&p?")
+    let data = fs::read_to_string(&filename)
+        .unwrap_or_else(|e| panic!("can't read {filename}: {e}"))
         .to_lowercase();
 
     let re = Regex::new(r"[a-z]{2,}").unwrap();
@@ -33,11 +35,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         SortMultipleOptions::new().with_order_descending(true),
     )?;
 
+    let word_col = ordered.column("words")?.str()?;
+    let count_col = ordered.column("words_count")?.u32()?;
+
     for i in 0..25 {
-        if let Ok(Row(r)) = ordered.get_row(i) {
-            if let [String(k), UInt32(n)] = r.as_slice() {
-                println!("{} - {}", k, n);
-            }
+        if let (Some(k), Some(n)) = (word_col.get(i), count_col.get(i)) {
+            println!("{} - {}", k, n);
         }
     }
 
